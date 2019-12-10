b542977ed6b29c2bb63261f6e07e25cb789ab8d1
surtweig <surtweig@gmail.com>
2019-12-10 13:20:26 +0200

    parallel: added matrix-csharp

---
+++ parallel/matrix-csharp/Matrix.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+namespace matrixcsharp
+{
+    public class Matrix
+    {
+        private double[][] data;
+
+        /// <summary>
+        /// Number of rows
+        /// </summary>
+        public int M { get { return data.Length; } }
+
+        /// <summary>
+        /// Number of columns
+        /// </summary>
+        public int N { get { return data[0].Length; } }
+
+        public double this[int i, int j]
+        {
+            get
+            {
+                return data[i][j];
+            }
+            set
+            {
+                data[i][j] = value;
+            }
+        }
+
+        public Matrix(int sizeM, int sizeN)
+        {
+            data = new double[sizeM][];
+            for (int i = 0; i < M; ++i)
+                data[i] = new double[sizeN];
+        }
+
+        public void FillValue(double val)
+        {
+            for (int i = 0; i < M; ++i)
+                for (int j = 0; j < N; ++j)
+                    data[i][j] = val;
+        }
+
+        public void FillRandom()
+        {
+            Random rand = new Random();
+            for (int i = 0; i < M; ++i)
+            {
+                double x = rand.NextDouble();
+                for (int j = 0; j < N; ++j)
+                    data[i][j] = rand.NextDouble() + x;
+            }
+        }
+
+        private int rowcmp(double[] row1, double[] row2)
+        {
+            double sum1 = 0.0;
+            double sum2 = 0.0;
+            for (int j = 0; j < N; ++j)
+            {
+                sum1 += row1[j];
+                sum2 += row2[j];
+            }
+            if (Math.Abs(sum2 - sum1) < double.Epsilon)
+                return 0;
+            if (sum2 < sum1)
+                return -1;
+            return 1;
+        }
+
+        public void Sort()
+        {
+            Array.Sort<double[]>(data, new Comparison<double[]>((row1, row2) => rowcmp(row2, row1)));
+        }
+
+        public void Add(Matrix other)
+        {
+            for (int i = 0; i < M; ++i)
+                for (int j = 0; j < N; ++j)
+                    this[i, j] += other[i, j];
+        }
+
+        public Matrix Multiply(Matrix other)
+        {
+            if (N != other.M)
+                throw new ArgumentException(String.Format("A.N = {0} != B.M = {1}", N, other.M));
+
+            Matrix mult = new Matrix(M, other.N);
+            for (int i = 0; i < mult.M; ++i)
+            {
+                for (int j = 0; j < mult.N; ++j)
+                {
+                    double sum = 0.0;
+                    for (int k = 0; k < N; ++k)
+                        sum += this[i, k] * other[k, j];
+                    mult[i, j] = sum;
+                }
+            }
+            return mult;
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < M; ++i)
+            {
+                for (int j = 0; j < N; ++j)
+                    Console.Write(String.Format("{0:0.00}", data[i][j]) + "\t");
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ parallel/matrix-csharp/Program.cs
@@ -0,0 +1,104 @@
+﻿//#define FORCE_SINGLE_THREAD
+using System.Threading;
+using System;
+
+namespace matrixcsharp
+{
+    class MainClass
+    {
+        public const int msize = 5;
+        public static object printLock = new object();
+
+        public static void Main(string[] args)
+        {
+            /*
+            Console.WriteLine("Hello World!");
+            Matrix m = new Matrix(5, 7);
+            m.FillRandom();
+            m.Sort();
+            m.Print();
+            */
+#if FORCE_SINGLE_THREAD
+            F1();
+            F2();
+            F3();
+#else
+            Thread tf1 = new Thread(F1);
+            tf1.Start();
+            Thread tf2 = new Thread(F2);
+            tf2.Start();
+            Thread tf3 = new Thread(F3);
+            tf3.Start();
+
+            tf1.Join();
+            tf2.Join();
+            tf3.Join();
+#endif
+        }
+
+        // 1.5 C = SORT(A) * (MA*ME) + SORT(B)
+        public static void F1()
+        {
+            Matrix A = new Matrix(msize, 1);
+            Matrix B = new Matrix(msize, 1);
+            Matrix MA = new Matrix(msize, msize);
+            Matrix ME = new Matrix(msize, msize);
+            A.FillRandom();
+            B.FillRandom();
+            MA.FillRandom();
+            ME.FillRandom();
+            A.Sort();
+            B.Sort();
+            Matrix AE = MA.Multiply(ME);
+            Matrix C = AE.Multiply(A);
+            C.Add(B);
+            lock (printLock)
+            {
+                Console.WriteLine("F1():");
+                C.Print();
+                Console.WriteLine();
+            }
+        }
+
+        // 2.5 MG = SORT(MF) * MK + ML
+        public static void F2()
+        {
+            Matrix MF = new Matrix(msize, msize);
+            Matrix MK = new Matrix(msize, msize);
+            Matrix ML = new Matrix(msize, msize);
+            MF.FillRandom();
+            MK.FillRandom();
+            ML.FillRandom();
+
+            MF.Sort();
+            Matrix MG = MF.Multiply(MK);
+            MG.Add(ML);
+            lock (printLock)
+            {
+                Console.WriteLine("F2():");
+                MG.Print();
+                Console.WriteLine();
+            }
+        }
+
+        // 3.5 O = (SORT(MP*MR)*S)
+        public static void F3()
+        {
+            Matrix MP = new Matrix(msize, msize);
+            Matrix MR = new Matrix(msize, msize);
+            Matrix S = new Matrix(msize, 1);
+            MP.FillRandom();
+            MR.FillRandom();
+            S.FillRandom();
+            Matrix MPR = MP.Multiply(MR);
+            MPR.Sort();
+            Matrix O = MPR.Multiply(S);
+            lock (printLock)
+            {
+                Console.WriteLine("F3():");
+                O.Print();
+                Console.WriteLine();
+            }
+        }
+    }
+}
