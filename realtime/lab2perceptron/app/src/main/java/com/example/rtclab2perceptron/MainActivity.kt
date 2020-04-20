8a6674c1dba6f9ece7f7dbf06c864ae3951892bf
surtweig <surtweig@gmail.com>
2020-04-20 03:47:51 +0300

    rtc: lab2 app update

--- realtime/lab2perceptron/app/src/main/java/com/example/rtclab2perceptron/MainActivity.kt
+++ realtime/lab2perceptron/app/src/main/java/com/example/rtclab2perceptron/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,6 +28,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(myCanvasView)
         */
 
+        spaceView.AddPoint(1f, 5f, 1, false)
+        spaceView.AddPoint(2f, 4f, 0, false)
+    }
+
+    public fun onAutoClick(v: View?) {
+        spaceView.AutoRedraw = AutoCheckBox.isChecked
     }
 
     public fun onStartClick(v: View?) {
@@ -36,18 +43,25 @@ class MainActivity : AppCompatActivity() {
         spaceView.AddPoint(300f, 300f, 1)
         spaceView.AddPoint(100f, 300f, 1)
         */
+        spaceView.Sigma = LearningRateEdit.text.toString().toFloat()
+        spaceView.Iters = IterationsCountEdit.text.toString().toInt()
+        spaceView.P = ThresholdEdit.text.toString().toFloat()
+        spaceView.startLearning()
+        LearnResultText.setText("W=(%.2f, %.2f)".format(spaceView.Weight.posX, spaceView.Weight.posY))
     }
 
     public fun onAddPosClick(v: View?) {
         val h = spaceView.height
         val w = spaceView.width
-        spaceView.AddPoint(w*0.5f, h*0.5f, 1)
+        spaceView.AddPoint(Random.nextFloat()*spaceView.spaceWidth*0.8f+0.1f,
+                           Random.nextFloat()*spaceView.spaceHeight*0.8f+0.1f, 1)
     }
 
     public fun onAddNegClick(v: View?) {
         val h = spaceView.height
         val w = spaceView.width
-        spaceView.AddPoint(w*0.5f, h*0.5f, 0)
+        spaceView.AddPoint(Random.nextFloat()*spaceView.spaceWidth*0.8f+0.1f,
+            Random.nextFloat()*spaceView.spaceHeight*0.8f+0.1f, 0)
     }
 
     public fun onClearClick(v: View?) {
--- realtime/lab2perceptron/app/src/main/java/com/example/rtclab2perceptron/MyCanvasView.kt
+++ realtime/lab2perceptron/app/src/main/java/com/example/rtclab2perceptron/MyCanvasView.kt
@@ -9,9 +9,10 @@ import android.view.MotionEvent
 import android.view.View
 import android.view.ViewConfiguration
 import androidx.core.content.res.ResourcesCompat
+import kotlin.math.abs
 
 // Stroke width for the the paint.
-private const val STROKE_WIDTH = 12f
+private const val STROKE_WIDTH = 6f
 
 /**
  * Custom view that follows touch events to draw on a canvas.
@@ -20,29 +21,56 @@ class MyCanvasView(context: Context, attrs: AttributeSet) : View(context, attrs)
 
     data class Point(var posX: Float = 0f, var posY: Float = 0f, var color: Int = 0)
     private val pointRadius: Float = 48f
+    public val spaceWidth: Float = 10f
+    public val spaceHeight: Float = 10f
     private val points: MutableList<Point> = mutableListOf<Point>()
 
-    public fun AddPoint(X: Float, Y: Float, C: Int) {
+    public fun AddPoint(X: Float, Y: Float, C: Int, redraw: Boolean = true) {
         points.add(Point(X, Y, C))
-        drawPoints()
+        if (redraw) {
+            drawPoints()
+            invalidate()
+        }
     }
 
     public fun ClearPoints() {
         points.clear()
         drawPoints()
+        invalidate()
+    }
+
+    public fun ViewToSpaceX(X: Float): Float {
+        return (X/width)*spaceWidth
+    }
+
+    public fun ViewToSpaceY(Y: Float): Float {
+        return (1f - Y/height)*spaceHeight
+    }
+
+    public fun SpaceToViewX(X: Float): Float {
+        return (X/spaceWidth)*width
+    }
+
+    public fun SpaceToViewY(Y: Float): Float {
+        return (1f - Y/spaceHeight)*height
     }
 
     private fun drawPoints() {
         for (p in points.iterator()) {
+            val tpx = SpaceToViewX(p.posX)
+            val tpy = SpaceToViewY(p.posY)
             if (p.color == 0)
-                extraCanvas.drawCircle(p.posX, p.posY, pointRadius, pointNegPaint)
+                extraCanvas.drawCircle(tpx, tpy, pointRadius, pointNegPaint)
             else
-                extraCanvas.drawCircle(p.posX, p.posY, pointRadius, pointPosPaint)
+                extraCanvas.drawCircle(tpx, tpy, pointRadius, pointPosPaint)
+            //val pf = perceptf(p)
+            //extraCanvas.drawText("%.0f".format(pf), tpx, tpy, textPaint)
         }
     }
 
     private fun pickPoint(X: Float, Y: Float): Point? {
-        val pr2 = pointRadius*pointRadius
+        val pr = (pointRadius/width)*spaceWidth
+        val pr2 = pr*pr
         for (p in points.iterator()) {
             val dX = p.posX - X
             val dY = p.posY - Y
@@ -50,6 +78,38 @@ class MyCanvasView(context: Context, attrs: AttributeSet) : View(context, attrs)
                 return p
         }
         return null
+    }
+
+    public var AutoRedraw: Boolean = false
+    public var P: Float = 4f
+    public var Sigma: Float = 0.1f
+    public var Iters: Int = 100
+    public var Weight: Point = Point(0f, 0f, 0)
+
+    private fun perceptf(inp: Point): Float {
+        val dot = inp.posX*Weight.posX + inp.posY*Weight.posY
+        if (dot > P)
+            return 1f
+        else
+            return 0f
+    }
+
+    private fun learnStep(inp: Point) {
+        val yc = perceptf(inp)
+        val yd = inp.color
+        val d = Sigma*(yd-yc)
+        Weight.posX += d*inp.posX
+        Weight.posY += d*inp.posY
+    }
+
+    public fun startLearning() {
+        Weight = Point(0f, 0f, 0)
+        for (i in 1..Iters) {
+            for (p in points.iterator()) {
+                learnStep(p)
+            }
+        }
+        invalidate()
     }
 
     // Holds the path you are currently drawing.
@@ -64,16 +124,30 @@ class MyCanvasView(context: Context, attrs: AttributeSet) : View(context, attrs)
     private lateinit var frame: Rect
 
     // Set up the paint with which to draw.
-    private val paint = Paint().apply {
+    private val textPaint = Paint().apply {
+        color = drawColor
+        // Smooths out edges of what is drawn without affecting shape.
+        isAntiAlias = true
+        // Dithering affects how colors with higher-precision than the device are down-sampled.
+        isDither = true
+        //style = Paint.Style.STROKE // default: FILL
+        //strokeJoin = Paint.Join.ROUND // default: MITER
+        //strokeCap = Paint.Cap.ROUND // default: BUTT
+        strokeWidth = STROKE_WIDTH // default: Hairline-width (really thin)
+        textSize = 64f
+    }
+
+    private val outlinePaint = Paint().apply {
         color = drawColor
         // Smooths out edges of what is drawn without affecting shape.
         isAntiAlias = true
         // Dithering affects how colors with higher-precision than the device are down-sampled.
         isDither = true
         style = Paint.Style.STROKE // default: FILL
-        strokeJoin = Paint.Join.ROUND // default: MITER
-        strokeCap = Paint.Cap.ROUND // default: BUTT
+        //strokeJoin = Paint.Join.ROUND // default: MITER
+        //strokeCap = Paint.Cap.ROUND // default: BUTT
         strokeWidth = STROKE_WIDTH // default: Hairline-width (really thin)
+
     }
 
     private val pointPosPaint = Paint().apply {
@@ -95,7 +169,7 @@ class MyCanvasView(context: Context, attrs: AttributeSet) : View(context, attrs)
      * If the finger has has moved less than this distance, don't draw. scaledTouchSlop, returns
      * the distance in pixels a touch can wander before we think the user is scrolling.
      */
-    private val touchTolerance = ViewConfiguration.get(context).scaledTouchSlop
+    private val touchTolerance = ViewConfiguration.get(context).scaledTouchSlop*0.25f
 
     private var currentX = 0f
     private var currentY = 0f
@@ -121,11 +195,94 @@ class MyCanvasView(context: Context, attrs: AttributeSet) : View(context, attrs)
         frame = Rect(inset, inset, width - inset, height - inset)
     }
 
+    private fun drawExtra() {
+        extraCanvas.drawColor(backgroundColor)
+        if (selectedPoint != null) {
+            selectedPoint?.posX = ViewToSpaceX(currentX)
+            selectedPoint?.posY = ViewToSpaceY(currentY)
+            drawPoints()
+            val label = "(%.2f, %.2f)".format(selectedPoint?.posX, selectedPoint?.posY)//selectedPoint?.posX.toString() + ", " + selectedPoint?.posY.toString()
+            var toffx: Float = 64f
+            var toffy: Float = -64f
+            var tbounds: Rect = Rect(0, 0, 0, 0)
+            textPaint.getTextBounds(label, 0, label.length, tbounds)
+            if (currentX + toffx + tbounds.width() > width-4f)
+                toffx = width-4f-currentX-tbounds.width()
+            if (currentY + toffy - tbounds.height() < 4f)
+                toffy = 64f+tbounds.height()
+            extraCanvas.drawText(label, currentX+toffx, currentY+toffy, textPaint)
+            val tpx = SpaceToViewX(selectedPoint!!.posX)
+            val tpy = SpaceToViewY(selectedPoint!!.posY)
+            extraCanvas.drawCircle(tpx, tpy, pointRadius, outlinePaint)
+
+            // w1x + w2y + b = 0
+            // x = (- w2y - b)/w1
+            // y = (- w1x - b)/w2
+            /*
+
+                x = 0:
+                    y >= 0 && y <= h
+                    y = -b/w2
+
+             */
+
+
+            /*
+            val wdx = Weight.posY
+            val wdy = Weight.posX
+            val wcx = P*Weight.posX
+            val wcy = P*Weight.posY
+            val hl = P+spaceWidth
+            extraCanvas.drawLine(wcx-wdx*hl, wcy-wdy*hl, wcx+wdx*hl, wcy+wdy*hl, outlinePaint)
+            */
+        }
+        else
+            drawPoints()
+
+        var wp0: Point? = null
+        var wp1: Point? = null
+        if (abs(Weight.posY) > 0.00001f) {
+            val y0 = P/Weight.posY
+            val yw = (-Weight.posX*spaceWidth + P)/Weight.posY
+            if (y0 >= 0f && y0 <= spaceHeight) {
+                wp0 = Point(0f, y0, 0)
+            }
+            if (yw >= 0f && yw <= spaceHeight) {
+                if (wp0 == null)
+                    wp0 = Point(spaceWidth, yw, 0)
+                else
+                    wp1 = Point(spaceWidth, yw, 0)
+            }
+        }
+        if (wp1 == null && abs(Weight.posX) > 0.00001f) {
+            val x0 = P/Weight.posX
+            val xh = (-Weight.posY*spaceHeight + P)/Weight.posX
+            if (x0 >= 0f && x0 <= spaceWidth) {
+                if (wp0 == null)
+                    wp0 = Point(x0, 0f, 0)
+                else
+                    wp1 = Point(x0, 0f, 0)
+            }
+            if (xh >= 0f && xh <= spaceWidth) {
+                if (wp0 == null)
+                    wp0 = Point(xh, spaceHeight, 0)
+                else
+                    wp1 = Point(xh, spaceHeight, 0)
+            }
+        }
+
+        if (wp0 != null && wp1 != null) {
+            extraCanvas.drawLine(SpaceToViewX(wp0.posX), SpaceToViewY(wp0.posY),
+                SpaceToViewX(wp1.posX), SpaceToViewY(wp1.posY), outlinePaint)
+        }
+    }
+
     override fun onDraw(canvas: Canvas) {
         // Draw the bitmap that has the saved path.
+        drawExtra()
         canvas.drawBitmap(extraBitmap, 0f, 0f, null)
         // Draw a frame around the canvas.
-        extraCanvas.drawRect(frame, paint)
+        //extraCanvas.drawRect(frame, paint)
     }
 
     /**
@@ -156,7 +313,7 @@ class MyCanvasView(context: Context, attrs: AttributeSet) : View(context, attrs)
         path.moveTo(motionTouchEventX, motionTouchEventY)
         currentX = motionTouchEventX
         currentY = motionTouchEventY
-        selectedPoint = pickPoint(currentX, currentY)
+        selectedPoint = pickPoint(ViewToSpaceX(currentX), ViewToSpaceY(currentY))
     }
 
     private fun touchMove() {
@@ -171,12 +328,12 @@ class MyCanvasView(context: Context, attrs: AttributeSet) : View(context, attrs)
             // Draw the path in the extra bitmap to save it.
             //extraCanvas.drawPath(path, paint)
         }
-        if (selectedPoint != null) {
-            selectedPoint?.posX = currentX
-            selectedPoint?.posY = currentY
-        }
-        extraCanvas.drawColor(backgroundColor)
-        drawPoints()
+
+        if (AutoRedraw)
+            startLearning()
+
+        drawExtra()
+
         // Invalidate() is inside the touchMove() under ACTION_MOVE because there are many other
         // types of motion events passed into this listener, and we don't want to invalidate the
         // view for those.
