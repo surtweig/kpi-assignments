eef946e979863c2e37585a8423149b95fe3f7fec
surtweig <surtweig@gmail.com>
2020-05-30 18:26:39 +0300

    rtc: dsp1 plotdraw

--- realtime/dsp/dsplab1/plotdraw.cpp
+++ realtime/dsp/dsplab1/plotdraw.cpp
@@ -4,3 +4,49 @@ PlotDraw::PlotDraw(QWidget *parent) : QWidget(parent)
 {
 
 }
+
+void PlotDraw::SetBuffer(quint32 samplesCount, quint32 signalsCount)
+{
+
+}
+
+void PlotDraw::ClearBuffer()
+{
+
+}
+
+void PlotDraw::AddFrame(float*)
+{
+
+}
+
+void PlotDraw::AddSample(float sample, quint32 signalIndex)
+{
+
+}
+
+void PlotDraw::SetView(quint32 firstIndex, quint32 lastIndex)
+{
+
+}
+
+void PlotDraw::SetView(quint32 firstIndex, quint32 lastIndex,
+             float low, float high)
+{
+
+}
+
+void PlotDraw::SetBackground(QColor color)
+{
+
+}
+
+void PlotDraw::SetSignalStyle(QColor color, float thickness)
+{
+
+}
+
+void PlotDraw::paintEvent(QPaintEvent* event)
+{
+
+}
--- realtime/dsp/dsplab1/plotdraw.h
+++ realtime/dsp/dsplab1/plotdraw.h
@@ -2,12 +2,37 @@
 #define PLOTDRAW_H
 
 #include <QWidget>
+#include <QPainter>
+#include <QList>
 
 class PlotDraw : public QWidget
 {
     Q_OBJECT
 public:
     explicit PlotDraw(QWidget *parent = nullptr);
+
+    void SetBuffer(quint32 samplesCount, quint32 signalsCount);
+    void ClearBuffer();
+    void AddFrame(float*);
+    void AddSample(float sample, quint32 signalIndex);
+
+    void SetView(quint32 firstIndex, quint32 lastIndex);
+    void SetView(quint32 firstIndex, quint32 lastIndex,
+                 float low, float high);
+
+    void SetBackground(QColor color);
+    void SetSignalStyle(QColor color, float thickness);
+
+    void paintEvent(QPaintEvent* event);
+
+private:
+    float* buffer;
+    QBrush bg;
+    QList<QPen> signalStyles;
+    quint32 viewFirst;
+    quint32 viewLast;
+    float viewLow;
+    float viewHigh;
 
 signals:
 
